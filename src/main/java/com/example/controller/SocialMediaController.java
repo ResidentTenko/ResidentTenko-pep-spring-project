fd4ce6ca03aee051b7d5cbbdd18edac255768bb9
Fidel Ojimba <fidelojimba@gmail.com>
2024-03-26 01:18:28 +0000

    Testing 2

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -23,34 +23,38 @@ public class SocialMediaController {
     private MessageService messageService;
 
     /**
-    * Handler to register a new account.
-    * @param account The new account that needs to be registered.
-    * @return on succcess: An Account object wrapped in a ResponseEntity as the response body to the HTTP caller.
-    * @return on failure: returns UnauthorizedException
-    */
+     * Handler to register a new account.
+     * @param account - the new account that needs to be registered.
+     * @return on success - an Account object wrapped in a ResponseEntity as the response body to the HTTP caller.
+     * @return on failure due to duplicate username - returns a ResponseEntity with a status of HttpStatus.CONFLICT (409).
+     * @return on failure due to invalid account - returns a ResponseEntity with a status of HttpStatus.BAD_REQUEST (400).
+     */
     @PostMapping("/register")
     public ResponseEntity<Account> registerUser(@RequestBody Account account) {
         try 
         {
             Account response = accountService.registerAccount(account);
+            // return the account in the response body if all goes well 
             return ResponseEntity.ok(response);
         } 
         catch (DuplicateUsernameException e) 
         {
+            // return error code 409 if a duplicate account exists
             return ResponseEntity.status(HttpStatus.CONFLICT).build();
         } 
         catch (InvalidAccountException e)
         {
+            // return error code 400 for all other errors
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
         }
     }
 
     /**
-    * Handler for user logins.
-    * @param account The new account that needs to be registered.
-    * @return on succcess: An Account object wrapped in a ResponseEntity as the response body to the HTTP caller.
-    * @return on failure: returns UnauthorizedException
-    */              
+     * Handler to login a user.
+     * @param account - the account that needs to be logged in.
+     * @return on succcess - an Account object wrapped in a ResponseEntity as the response body to the HTTP caller.
+     * @return on failure due to unauthorized login details - returns a ResponseEntity with a status of HttpStatus.UNAUTHORIZED (401).
+     */              
     @PostMapping("/login")
     public ResponseEntity<Account> loginUser(@RequestBody Account account) 
     {
@@ -65,29 +69,52 @@ public class SocialMediaController {
         }
     }
 
+    /**
+     * Handler to post a new message.
+     * @param message - the message that needs to be posted.
+     * @return on success - a Message object wrapped in a ResponseEntity as the response body to the HTTP caller.
+     * @return on failure due to invalid message - returns a ResponseEntity with a status of HttpStatus.BAD_REQUEST (400).
+     */
     @PostMapping("/messages")
     public ResponseEntity<Message> postMessage(@RequestBody Message message) {
-        try {
+        try
+        {
             Message savedMessage = messageService.submitMessage(message);
             return ResponseEntity.ok(savedMessage);
-        } catch (InvalidMessageException e) {
-            // Handle validation errors
+        } 
+        catch (InvalidMessageException e)
+        {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
         }
     }
 
+    /**
+     * Handler to get all messages.
+     * @return a list of Message objects wrapped in a ResponseEntity as the response body to the HTTP caller.
+     */
     @GetMapping("/messages")
     public ResponseEntity<List<Message>> getAllMessages() {
         List<Message> allMessages = messageService.getAllMessages();
         return ResponseEntity.ok(allMessages);
     }
 
+    /**
+     * Handler to get a specific message by its id.
+     * @param message_id - the id of the message to be retrieved.
+     * @return the Message object with the given id wrapped in a ResponseEntity as the response body to the HTTP caller.
+     */
     @GetMapping("/messages/{message_id}")
     public ResponseEntity<Message> getMessageById(@PathVariable Integer message_id) {
         Message message = messageService.getMessageById(message_id);
         return ResponseEntity.ok(message);
     }
 
+    /**
+     * Handler to delete a specific message by its id.
+     * @param message_id - the id of the message to be deleted.
+     * @return If no rows were affected - an empty ResponseEntity with a status of HttpStatus.OK (200).
+     * @return If the message was successfully deleted - the number of rows affected wrapped in a ResponseEntity as the response body to the HTTP caller.
+     */
     @DeleteMapping("/messages/{message_id}")
     public ResponseEntity<?> deleteMessageById(@PathVariable Integer message_id) {
         long rowsAffected = messageService.deleteMessageById(message_id);
@@ -101,6 +128,13 @@ public class SocialMediaController {
         }
     }
 
+    /**
+     * Handler to update a specific message by its id.
+     * @param message_id - the id of the message to be updated.
+     * @param message - the new content for the message.
+     * @return on successful update - a ResponseEntity with a status of HttpStatus.OK (200) and a body of 1.
+     * @return on failed update - a ResponseEntity with a status of HttpStatus.BAD_REQUEST (400).
+     */
     @PatchMapping("/messages/{message_id}")
     public ResponseEntity<?> updateMessageByIdHandler(@PathVariable Integer message_id, @RequestBody Message message) {
         // update the id first just in case
@@ -116,6 +150,11 @@ public class SocialMediaController {
         }
     }
 
+    /**
+     * Handler to get all messages posted by a specific account.
+     * @param account_id The id of the account whose messages are to be retrieved.
+     * @return A list of Message objects posted by the account, wrapped in a ResponseEntity as the response body to the HTTP caller.
+     */
     @GetMapping("/accounts/{account_id}/messages")
     public ResponseEntity<List<Message>> getMessagesByAccountId(@PathVariable Integer account_id) {
         List<Message> messages = messageService.getMessagesByAccountId(account_id);
